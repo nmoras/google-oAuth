2d9aca0d7e3e1d09ffe520f9fd20744b657cb449
poem <normamoras@gmail.com>
2020-09-02 07:05:28 -0400

    updates gitignore

---
+++ config/oAuth.js
@@ -0,0 +1,13 @@
+const cors = require('cors');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth');
+const key = require('./key')
+
+passport.use(
+    new GoogleStrategy({
+    //options for the Google strat
+    clientId: key.google.clientId,
+    clientSecret: key.google.clientSecret
+}), ()=>{
+    //passport callback function
+})
